49703fba2234f5905ce1e60c547ce9b10ecd1cc9
Pavel Kadantsev <p.a.kadantsev@gmail.com>
2017-08-03 12:19:17 +0300

    started 4_json

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,23 @@
 import json
+import os
+import argparse
+from pprint import pprint
+
+def load_data():
+    if os.path.isfile("raw_json.json"): 
+        with open("raw_json.json", 'r', encoding='utf-8-sig') as raw_json_file:
+            raw_json_file_text = json.load(raw_json_file)
+            #return raw_json_file_text
+            pprint(raw_json_file_text)
+    else:
+        print("oops")
+        return None
+
+load_data()       
+
+# def pretty_print_json(data):
+#     pass
 
 
-def load_data(filepath):
-    pass
-
-
-def pretty_print_json(data):
-    pass
-
-
-if __name__ == '__main__':
-    pass
+# if __name__ == '__main__':
+#     pass
