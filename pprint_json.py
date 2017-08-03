f9c5ed8e7fe19944550439ad7ebccd9d077bfd16
Pavel Kadantsev <p.a.kadantsev@gmail.com>
2017-08-03 13:57:09 +0300

    Небольшие изменения...

--- pprint_json.py
+++ pprint_json.py
@@ -3,25 +3,27 @@ import os
 import argparse
 from pprint import pprint
 
-def load_data():
-    if os.path.isfile("raw_json.json"): 
-        with open("raw_json.json", 'r', encoding='utf-8') as raw_json_file:
+def load_data(filepath):
+    if os.path.isfile(filepath): 
+        with open(filepath, 'r', encoding='utf-8') as raw_json_file:
             raw_json_file_text = json.loads(raw_json_file)
-            #return raw_json_file_text
-            pprint(raw_json_file_text)
+            return raw_json_file_text
+            #pprint(raw_json_file_text)
     else:
         print("oops")
         return None
-
-load_data()       
 
 def pretty_print_json(data):
     print(json.dumps(data, indent=4, ensure_ascii=False))
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParse('description' = 'Getting pretty json')
-    parser.add_argument('-p', '--path', required = True,        
-                        'help' = 'Enter path to json file after flag -p or --path')
-    namespace = parser.parse_args(sys.argv[1:])
-    
+    parser = argparse.ArgumentParser(description = 'Getting pretty json')
+    parser.add_argument('-p', '--path', required = True, default = os.getcwd(),        
+                        help = 'Enter path to json file after flag -p or --path')
+    namespace = parser.parse_args()
+
+    if namespace.path:
+        pretty_print_json(load_data(namespace.path))
+
+
 
